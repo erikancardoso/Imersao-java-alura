191387add59f9c89870da02d4107dd16891af704
erikancardoso <erikancardoso@outlook.com>
2022-08-17 01:59:54 -0400

    resumo

--- aulas-praticas/src/imersao/java/api/App.java
+++ aulas-praticas/src/imersao/java/api/App.java
@@ -13,26 +13,19 @@ public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
-        //fazer conecção http e buscar informações na API"
-        //guardando link da API que desejo acessar
+        //fazer conexão http e buscar informações na API"
         String url = "https://api.mocki.io/v2/549a5d8b";
-        //crio um URI (permitir a interação com representações do recurso através de uma rede)
         URI local = URI.create(url);
-        //criando um acesso http para o cliente
         HttpClient client = HttpClient.newHttpClient();
-        //crio uma requisição para acessar o endereço, buscando dados, etc
         HttpRequest request = HttpRequest.newBuilder(local).GET().build();
-        //crio um objeto de resposta para enviar a (requisição do cliente e receber o tipo do arquivo)
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); //BodyHandler retorna uma forma de leitura de dados, string, boolean, etc
-        //criar uma variavel para guardar as informações de resposta requeridas.
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
         //imprimir as imformações gardadas dentro do corpo do documento
         //System.out.println(body);
 
         //Estrair dados (title, image, rank")
-        //preciso primeiro criar um metodo que vai parsear e identificar expressoes regulares para poder colocar dentro da list
-        var parser = new JsonParse(); // para não ficar repetindo pode usar o var no ligar da biblioteca
-        //criar uma lista para separar os tipos de dados que preciso armazenar
+        
+        var parser = new JsonParse();
         List<Map<String, String>> filmList = parser.parse(body);
 
         //manipular dados da api
@@ -42,17 +35,13 @@ public class App {
         for (Map<String, String> verFilme: filmList) {
             System.out.println(verFilme.get("title"));
         }
-        System.out.println("\nExibir Titulo, Rank e imagem");
+        System.out.println("\n \u001b[1;39m \u001b[45m Exibir Titulo, Rank e imagem \u001b[m");
         for (Map<String, String> verTodos: filmList) {
-            System.out.println(verTodos.get("title"));
-            System.out.println(verTodos.get("imDbRating"));
-            System.out.println(verTodos.get("image"));
+            System.out.println("\u001b[1;31m \u001b[46m "+verTodos.get("title")+" \u001b[m");
+            System.out.println("\u001b[1;36m \u001b[105m \uD83D\uDC99 "+verTodos.get("imDbRating")+" \uD83D\uDC99 \u001b[m");
+            System.out.println("\u001b[107r4m "+verTodos.get("image")+"\u001b[m");
             System.out.println();
         }
         System.out.println("Ordenar por classificação");
-
-
     }
-
-
 }
